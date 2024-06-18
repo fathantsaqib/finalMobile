0eb479e1acfb656f632e0d1983120543b38d7267
fathantsaqib <fathants12@gmail.com>
2024-06-18 17:20:52 +0800

    H071221099_finalmobile

---
+++ lib/splash.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(size: 150),
+            SizedBox(height: 200),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(300, 50),
+                backgroundColor: const Color.fromARGB(255, 23, 96, 156),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
